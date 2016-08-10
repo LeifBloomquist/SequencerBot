19db55d439ac088577c2f936898137495db283de
Leif Bloomquist <github@leifbloomquist.net>
2016-08-10 00:04:25 -0400

    Tweetinvi cleanly added with NuGet

---
+++ SequencerBotTest1/SequencerBotTest1/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace SequencerBotTest1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.OutputLabel.Location = new System.Drawing.Point(12, 9);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(498, 129);
+            this.OutputLabel.TabIndex = 2;
+            this.OutputLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 385);
+            this.Controls.Add(this.OutputLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label OutputLabel;
+    }
+}
+
---
+++ SequencerBotTest1/SequencerBotTest1/Form1.cs
@@ -0,0 +1,190 @@
+﻿using Sanford.Multimedia.Midi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tweetinvi;
+using Tweetinvi.Core.Exceptions;
+using Tweetinvi.Core.Interfaces;
+
+namespace SequencerBotTest1
+{
+    public partial class Form1 : Form
+    {
+        private OutputDevice outDevice = null;
+        private SynchronizationContext context;
+
+        private Boolean connected = false;
+
+        private System.Timers.Timer myTimer = new System.Timers.Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMIDI();
+            LoginandStart();
+            OutputLabel.Text = "Sequencer Bot ready.";
+        }
+
+        private void InitializeMIDI()
+        {
+            connected = false;
+
+            int devices = OutputDevice.DeviceCount;
+
+ 	        if (devices == 0)
+            {
+                MessageBox.Show("No MIDI output devices available.", "Warning!",  MessageBoxButtons.OK, MessageBoxIcon.Warning);                
+            }
+            else
+            {
+                try
+                {
+                    context = SynchronizationContext.Current;                    
+                   
+                    for (int i=0; i< devices; i++)
+                    {
+                        MidiOutCaps caps = OutputDevice.GetDeviceCapabilities(i);                        
+                        if (caps.name == "Generator")
+                        {
+                            ConnectMIDI(i);
+                            return;
+                        }
+                    }
+
+                    MessageBox.Show("Generator device not found!", "MIDI Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    Close();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "MIDI Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    Close();
+                }
+            }
+        }
+
+        private void ConnectMIDI(int selected)
+        {
+            if (outDevice != null)
+            {
+                outDevice.Close();
+            }
+
+            outDevice = new OutputDevice(selected);            
+            outDevice.Reset();            
+            connected = true;
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            //LoginandStart();
+        }
+
+        private void LoginandStart()
+        {
+            //Auth.SetUserCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
+            
+
+            try
+            {
+                Auth.SetUserCredentials("PjrQTBBxU792e09klffO8r4NO", "01VNgL6YjVrPe5dbHDkOepdxC23hsa32pqlL2xIGLGPyMAZfZr", "716827397945827328-98tFDNfFAFhqhG6DbT9eKjPOdTrHX6Q", "VMmPvX6zUy7NxiMUsGyRKYStHFPTPXztqL9o4cSKDc9DE");
+
+                var authenticatedUser = User.GetAuthenticatedUser();
+
+                if (authenticatedUser == null)
+                {
+                    MessageBox.Show("Could not authenticate user!");
+                    Environment.Exit(-1);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception authenticating user! " + ex.Message);
+                Environment.Exit(-2);
+            }
+
+            var stream = Stream.CreateUserStream();
+            stream.TweetCreatedByAnyoneButMe += stream_TweetReceived;
+            stream.MessageReceived += stream_MessageReceived;
+            stream.StartStreamAsync();           
+        }
+
+
+        private void stream_TweetReceived(object sender, Tweetinvi.Events.TweetReceivedEventArgs args)
+        {
+          String received_tweet = args.Tweet.ToString();
+
+          Invoke(new Action(() =>
+              this.OutputLabel.Text = "Tweet received: " + received_tweet
+          ));
+
+
+          ProcessCommand(received_tweet);
+
+          // ITweet thetweet = args.Tweet;
+          //      String message = "Tweet received from @" + thetweet.CreatedBy.ScreenName + " - ACKNOWLEDGED";
+          //      DoTweet(message);
+        }
+
+
+        private void stream_MessageReceived(object sender, Tweetinvi.Events.MessageEventArgs args)
+        {
+          String received_message = args.Message.ToString();
+
+
+          Invoke(new Action(() =>
+              this.OutputLabel.Text = "Tweet received: " + received_message
+          ));
+
+
+          ProcessCommand(received_message);
+
+          // ITweet thetweet = args.Tweet;
+          //      String message = "Tweet received from @" + thetweet.CreatedBy.ScreenName + " - ACKNOWLEDGED";
+          //      DoTweet(message);
+        }
+
+        private void ProcessCommand(string text)
+        {
+            text = text.ToLower();
+            string[] words = text.Split(' ');
+
+            for (int i=0; i < words.Length; i++)
+            {
+                if (words[i] == "volume") DoVolume(words[i+1]);
+                if (words[i] == "bpm") DoBPM(words[i+1]);                
+            }            
+        }
+
+        private void DoVolume(string v)
+        {
+            int vol = (int)long.Parse(v);
+            SendMIDI(ChannelCommand.Controller, 0, 1, vol);
+        }
+
+        private void DoBPM(string arg)
+        {
+            int bpm = (int)long.Parse(arg);
+            SendMIDI(ChannelCommand.Controller, 0, 2, bpm);
+        }
+   
+        private void DoTweet(String s)
+        {
+            var atweet = Tweet.PublishTweet(s);
+        }
+
+        private void SendMIDI(ChannelCommand command, int channel, int data1, int data2)
+        {
+            if (!connected) return;
+
+            ChannelMessage cm = new ChannelMessage(command, channel, data1, data2);
+            outDevice.Send(cm);
+        }
+    }
+}
